89802e1696b574ff577f7cb2483487a7396cc683
Kubilay <kblycaglayan@gmail.com>
2020-06-10 15:34:09 +0300

    Add scientist

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,4 +14,10 @@ module SessionsHelper
   def sign_in(user)
     session[:current_user_id] = user.id
   end
+
+  def sign_in_page_link
+    return if signed_in?
+
+    link_to 'Back to Sign In Page', new_session_path, class: 'btn btn-primary text-white'
+  end
 end
