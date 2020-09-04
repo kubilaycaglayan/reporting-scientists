730014bfc8dc65d442cadf83636a864fc8d3872d
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-04 17:51:55 +0300

    Add index controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,8 @@
 class API::UsersController < ApplicationController
-
+  def index
+    @users = Users.all
+    respond_to do |format|
+      format.json { render :json => @users }
+    end
+  end
 end
