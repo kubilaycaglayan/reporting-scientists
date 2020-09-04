92f92b3c08f6322a754a0e420742b64902368f4c
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-04 17:48:22 +0300

    Add subdomain to the api routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :followings, only: [:new, :create, :destroy]
   resources :opinions, only: [:index, :new, :create]
-  namespace :api do
+
+  namespace :api, :path => "", :constraints => {:subdomain => "api"} do
     resources :users
   end
+
   root to: 'sessions#new'
   get '*path' => redirect('/')
 end
